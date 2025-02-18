61130393fa070f727305ca364788785bbd0a7a74
Tomasz Zdanowicz <tom.zdan@protonmail.com>
2025-02-18 22:09:10 +0100

    section prices finished, added section's reviews html

--- js/main.js
+++ js/main.js
@@ -17,3 +17,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const btnsPrices = document.querySelectorAll(".offer__prices-btn");
+
+    btnsPrices.forEach((button) => {
+        button.addEventListener("click", function () {
+            const activeText = document.querySelector(".offer__prices-textbox.active");
+            const activeIcon = document.querySelector(".offer__prices-btn i.rotated");
+            const text = this.nextElementSibling;
+            const icon = this.querySelector("i");
+
+            if (activeText && activeText !== text) {
+                activeText.style.maxHeight = "0";
+                activeText.classList.remove("active");
+                if (activeIcon) {
+                    activeIcon.classList.remove("rotated");
+                }
+            }
+
+            if (!text.classList.contains("active")) {
+                text.classList.add("active");
+                text.style.maxHeight = text.scrollHeight + "px";
+                icon.classList.add("rotated");
+            } else {
+                text.style.maxHeight = "0";
+                text.classList.remove("active");
+                icon.classList.remove("rotated");
+            }
+        });
+    });
+});
