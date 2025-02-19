e209691e9ff79d0759d197858428b7766ba122ee
Tomasz Zdanowicz <tom.zdan@protonmail.com>
2025-02-19 22:23:18 +0100

    section reviews added

---
+++ js/slick.js
@@ -0,0 +1,30 @@
+// $(document).ready(function () {
+//     $('.reviews__textbox').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         autoplay: true,
+//         autoplaySpeed: 2500,
+//         arrows: false,
+//         mobileFirst: true,
+//         responsive: [{
+//             breakpoint: 992,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 1
+//             }
+//         }],
+
+//     });
+// })
+
+
+$('.reviews__textbox').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+
+document.querySelector(".slick-prev").innerHTML = '<i class="fa-sharp fa-solid fa-angle-left"></i><i class="fa-sharp fa-solid fa-angle-left"></i>';
+document.querySelector(".slick-next").innerHTML = '<i class="fa-sharp fa-solid fa-angle-right"></i><i class="fa-sharp fa-solid fa-angle-right"></i>';
